f57ee181e93019c947c9777438933a5cab5c2e77
Renzo Louman <loumanbooks@gmail.com>
2019-02-24 19:27:40 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,60 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.getElementById("random");
+var randomCl = document.getElementsByClassName("colors")
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+
+function getRandomColor1() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomColor2() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;;
+}
+
+
+function gradientColors(event) {
+	color1.value = getRandomColor1();
+	color2.value = getRandomColor2();
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", gradientColors);
+
